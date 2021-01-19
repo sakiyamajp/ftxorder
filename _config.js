36896b15fa131275206f484e35eab433949ee29b
saky <s@sssagi.net>
2021-01-20 04:27:38 +0900

    update sample config

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 export default {
-	subaccount : "develop",
-	key : "Pkq4gq34g3q4g3q4gq34gaadgasdgsgaC",
-	secret : "9fsdfsdfeaqg34q4qg34gq34yf0wO_X",
+	subaccount : "enter ftx sub account name",
+	key : "enter ftx api key",
+	secret : "enter ftx api secret",
 	port : 8081
 }
